eebe5a1e761358eab80ac0c9f8b7968a1724f8b7
Techteker <ranarajendar930@gmail.com>
2026-09-20 07:12:18 +0530

    fix: use JSON Capacitor config

--- capacitor.config.ts
+++
@@ -1,3 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-const config: CapacitorConfig = { appId: 'com.proximax.homeworkout', appName: 'Home Workout Pro', webDir: 'www', bundledWebRuntime: false };
-export default config;
